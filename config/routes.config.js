6f0a467704098b4bbf873a9b643c97cdce6763b8
luismunoz83 <javier@Castellanas-MBP.home>
2022-06-26 11:05:33 +0200

    Start lab

--- config/routes.config.js
+++ config/routes.config.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const router = express.Router();
 
+const tasks = require('../controllers/tasks.controller')
+
 // TODO: GET /tasks -> render task list with filters
-// TODO: GET /tasks/:id -> render task detail
+router.get('/tasks', tasks.list)
+
 // TODO: GET /tasks/new -> render task form
+
+// TODO: GET /tasks/:id -> render task detail
+router.get('/tasks/:id', tasks.detail)
+
 // TODO: POST /tasks -> create task and redirect to list
 // TODO: POST /tasks/:id/delete -> delete task and redirect to list
 // Check tasks.controller.js exported functions!
--- controllers/tasks.controller.js
+++ controllers/tasks.controller.js
@@ -1,21 +1,31 @@
+const { tasks } = require("../services/tasks.service");
 const Task = require("../services/tasks.service");
 
 module.exports.list = (req, res, next) => {
   // TODO: use Task.find() to get all tasks and render tasks list
   // Task.find() returns a promise that resolves with task list
-  res.send("TODO");
+  Task.find()
+    .then(tasks =>res.render('tasks/list', {tasks}))
+    .catch(error => console.log (error))
 };
 
 module.exports.detail = (req, res, next) => {
   // TODO: use Task.findById(id) to get task by id and render task detail
   // Task.findById() returns a promise that resolves with task detail (or undefined if it does not exist)
-  res.send("TODO");
+  const {id} = req.params
+  Task.findById(id)
+    .then(tasks => res.render('tasks/detail', {tasks}))
+    .catch(error => console.log (error))
 };
 
 module.exports.create = (req, res, next) => {
   // TODO: use Task.create({...}) to create a new Task and redirect to list
   // Task.create() returns a promise that resolves with created task detail
-  res.send("TODO");
+  const data = {title: req.body.title}
+
+  Task.create(data)
+    .then(task => res.redirect('/tasks'))
+    .catch(error => console.log(error))
 };
 
 module.exports.delete = (req, res, next) => {
